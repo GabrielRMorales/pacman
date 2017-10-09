8a16077dce2a01689e4d15c696071b81570d01a0
Gabriel Morales <grmorale@gmail.com>
2017-10-09 15:38:43 -0700

    Add Ghost and Game Over

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
   //3=<div class="ground"></div>
   //4=<div class="pacman"></div>
   //5=<div class="ghost"></div>
+
   var map=[
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,2,2,1,2,2,1,2,2,2,2,2,1,2,2,1,2,2,1],
     [1,2,1,1,2,1,1,2,1,2,1,2,1,1,2,1,1,2,1],
-    [1,2,2,2,2,1,2,2,2,2,2,2,2,1,2,2,2,2,1],
+    [1,2,2,2,2,1,2,2,2,5,2,2,2,1,2,2,2,2,1],
     [1,1,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,1,1],
     [1,2,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,2,1],
     [1,1,1,2,1,1,1,1,2,1,2,1,1,1,1,2,1,1,1],
@@ -16,17 +17,23 @@ $(document).ready(function(){
     [1,2,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,2,1],
     [1,2,2,2,2,1,2,2,2,4,2,2,2,1,2,2,2,2,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-  ];
-  var pacman={
+  ],
+  pacman={
     x: 9,
-    y: 10,
-  };
-  var score=0;
-    function drawWorld(){
+    y: 9,
+  },
+  ghost={
+    x:9,
+    y:3
+  },
+  score=0,
+  gameover=false;
+
+
+  function drawWorld(){
       document.getElementById("world").innerHTML="";
       for (var i=0;i<map.length;i++){
         for (var j=0;j<map[i].length;j++){
-          //console.log(map[i][j]);
           var block=map[i][j];
           if (block===1){
             $("#world").append("<div class='wall'></div>")
@@ -60,44 +67,81 @@ $(document).ready(function(){
         map[pacman.y][pacman.x]=4;
     }
 
-    document.onkeydown=function(e){
-      console.log(e.keyCode);
-      if (e.keyCode===37){
-        //left
-      if (map[pacman.y][pacman.x-1]!==1){ 
+    function getRandomIntInclusive() {
+        var min = Math.ceil(0),max = Math.floor(1);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      } 
+    function game_over_check(){
+      if(ghost.x==pacman.x && ghost.y==pacman.y){
+            $("#scoreboard").text("GAME OVER");
+            gameover=true;
+          }
+    }
+    function ghost_move(){
+      
+      var arr=[1,-1],hori_or_vert=getRandomIntInclusive(), direction=arr[getRandomIntInclusive()];
+
+      if (hori_or_vert==0){
+        if (map[ghost.y+direction][ghost.x]!==1){        
+          map[ghost.y][ghost.x]=3;
+          ghost.y+=direction;
+          game_over_check();
+          map[ghost.y][ghost.x]=5;
+        }
+      }
+      else if (hori_or_vert==1) { 
+        if (map[ghost.y][ghost.x+direction]!==1){        
+          map[ghost.y][ghost.x]=3;
+          ghost.x+=direction;
+          game_over_check();
+          map[ghost.y][ghost.x]=5;
+        }
+      }        
+     
+      }
+
+
+document.onkeydown=function(e){ 
+
+if (gameover==false){
+  
+  switch(e.keyCode){
+      case 37:
+      //left
+        if (map[pacman.y][pacman.x-1]!==1){ 
         score_up(-1);
         move(-1);        
-      }
+        }
+      break;
 
-      }
-
-      else if (e.keyCode===38){
-        //up
-      if (map[pacman.y-1][pacman.x]!==1){
+      case 38:
+       //up
+        if (map[pacman.y-1][pacman.x]!==1){
         score_up(0,-1);
         move(0,-1);        
-      }
+       }
+      break;
 
-      }
-
-      else if (e.keyCode===39){
+      case 39:
         //right
-      if (map[pacman.y][pacman.x+1]!==1){
+        if (map[pacman.y][pacman.x+1]!==1){
         score_up(1);  
         move(1);             
       }
+      break;
 
-      }
-      else if (e.keyCode===40){
+      case 40:
         //down
-      if (map[pacman.y+1][pacman.x]!==1){
+        if (map[pacman.y+1][pacman.x]!==1){
         score_up(0,+1);
         move(0,1);
       }
+      break;
+    }
+  }
+}
 
-      }
-      drawWorld();
-    };
-    drawWorld();
-
+    setInterval(ghost_move, 300);
+    setInterval(drawWorld,300); 
+  
 });
